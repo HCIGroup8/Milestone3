e4a3442318a43bb751d8fb5606b56f906e67b664
Cody <codyaauch@gmail.com>
2020-11-03 21:59:33 -0600

    Added divs for each page

---
+++ Logic.js
@@ -0,0 +1,10 @@
+function nextPageClick() {
+    var addr = document.getElementById("addrPage");
+    var prefs = document.getElementById("categoriesPage");
+    var rests = document.getElementById("restaurantsPage");
+    var menu = document.getElementById("menuPage");
+    var cart = document.getElementById("cartPage");
+
+    addr.style.display = "none";
+    prefs.style.display = "inline";
+}
