0c241171cadca848873c845b45d629dcac92c0da
Josh Daigle <56177903+JoshuaDaigle@users.noreply.github.com>
2020-11-07 23:24:49 -0600

    fixup

--- Logic.js
+++ Logic.js
@@ -1,19 +1,16 @@
-function catagoriesPageClick() {
-    var addr = document.getElementById("addrPage");
-    var prefs = document.getElementById("categoriesPage");
-    var rests = document.getElementById("restaurantsPage");
-    var menu = document.getElementById("menuPage");
-    var cart = document.getElementById("cartPage");
+var addr = document.getElementById("addrPage");
+var prefs = document.getElementById("categoriesPage");
+var rests = document.getElementById("restaurantsPage");
+var menu = document.getElementById("menuPage");
+var cart = document.getElementById("cartPage");
 
+function catagoriesPageClick() {
     addr.style.display = "none";
     prefs.style.display = "inline";
 }
 
 
 function resturantsPageClick() {
-    var prefs = document.getElementById("categoriesPage");
-    var rests = document.getElementById("restaurantsPage");
-
     prefs.style.display = "none";
     rests.style.display = "inline";
 }
