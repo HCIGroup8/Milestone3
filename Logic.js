7bf153c13cb38a5e204d9a1edc045174fc5c3c40
Cody <codyaauch@gmail.com>
2020-11-08 00:32:26 -0600

    Added some button functionality

--- Logic.js
+++ Logic.js
@@ -4,13 +4,22 @@ var rests = document.getElementById("restaurantsPage");
 var menu = document.getElementById("menuPage");
 var cart = document.getElementById("cartPage");
 
-function catagoriesPageClick() {
+function categoriesPageClick() {
     addr.style.display = "none";
     prefs.style.display = "inline";
 }
 
+function categoriesPageBack() {
+    prefs.style.display = "none";
+    addr.style.display = "inline";
+}
 
 function resturantsPageClick() {
     prefs.style.display = "none";
     rests.style.display = "inline";
+}
+
+function restaurantsPageBack() {
+    rests.style.display = "none";
+    prefs.style.display = "inline";
 }
