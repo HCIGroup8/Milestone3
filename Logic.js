d84d09663c5c4a46a5d62d002bfd27dce3b6700b
Cody <codyaauch@gmail.com>
2020-11-14 16:34:59 -0600

    minor changes

--- Logic.js
+++ Logic.js
@@ -1,8 +1,12 @@
+// Global Variables
+
+// Page Divs
 var addr = document.getElementById("addrPage");
 var prefs = document.getElementById("categoriesPage");
 var rests = document.getElementById("restaurantsPage");
 var menu = document.getElementById("menuPage");
 var cart = document.getElementById("cartPage");
+
 
 function categoriesPageClick() {
     addr.style.display = "none";
@@ -17,9 +21,28 @@ function categoriesPageBack() {
 function resturantsPageClick() {
     prefs.style.display = "none";
     rests.style.display = "inline";
+
+    getPreferences();
+
+
 }
 
 function restaurantsPageBack() {
     rests.style.display = "none";
     prefs.style.display = "inline";
+}
+
+function getPreferences() {
+    var fastFood = document.getElementById("fastfoodCB");
+    var pizza = document.getElementById("pizzaCB");
+
+    var fastFoodChecked = fastFood.checked();
+    var pizzaChecked = pizza.checked();
+
+    var prefs = {
+        ff: fastFoodChecked,
+        pi: pizzaChecked
+    };
+
+    return prefs;
 }
